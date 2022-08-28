d43f97626d966a121de67dc23370e29376fa2cf6
SoHenriquez <estebansotohenriquez@gmail.com>
2022-08-28 17:31:11 -0400

    Cambio en controlador insertar a PDO

--- grupo-7/controlador/hu4_conserjeria_controlador/hu4_insert.php
+++ grupo-7/controlador/hu4_conserjeria_controlador/hu4_insert.php
@@ -2,17 +2,24 @@
 require_once("../../bds/conexion.php");
 date_default_timezone_set('Chile/Continental');  
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    /* Información enviada por el formulario */
     $tipo = $_POST['tipo'];
     $titulo = $_POST['titulo'];
     $fecha = date('Y-m-d');
     $hora = date('H:i:s');
     $descripcion = $_POST['descripcion'];
+    /* Fin información enviada por el formulario */
     if(!empty($tipo && $titulo )){
-        $insertarSql = "INSERT INTO formulario(usuario_clave,tipo_form_clave, form_titulo, form_descripcion, form_fecha, form_hora) 
-        VALUES('3','$tipo','$titulo','$descripcion','$fecha','$hora');";
-        $insertar = mysqli_query($con,$insertarSql);
-
+        $insertarSql = "INSERT INTO formulario(formulario_titulo,formulario_tipo, formulario_fecha, formulario_hora, formulario_contenido) 
+        VALUES(:formulario_titulo,:formulario_tipo, :formulario_fecha, :formulario_hora, :formulario_contenido)";
         
+        $insertarSql = $bd->prepare($insertarSql);
+        $insertarSql->bindParam(':formulario_titulo',$titulo,PDO::PARAM_STR, 45);
+        $insertarSql->bindParam(':formulario_tipo',$tipo,PDO::PARAM_STR, 45);
+        $insertarSql->bindParam(':formulario_fecha',$fecha,PDO::PARAM_STR);
+        $insertarSql->bindParam(':formulario_hora',$hora,PDO::PARAM_STR);
+        $insertarSql->bindParam(':formulario_contenido',$descripcion,PDO::PARAM_STR, 500);
+        $insertarSql->execute();
     }else{
         echo "error se ingreso campo vacio";
     }
