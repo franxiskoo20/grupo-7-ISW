db9be17112a2e1e6f9e1ca389036714e70b831d6
SoHenriquez <estebansotohenriquez@gmail.com>
2022-08-31 16:13:41 -0400

    Se agrega alerta al eliminar un registro y se modifica la antigua alerta de agregar

--- grupo-7/js/alerta_agregar.js
+++ grupo-7/js/alerta_agregar.js
@@ -2,12 +2,12 @@ function validar_formulario_publicar() {
     event.preventDefault();
     Swal.fire({
         title: '¿Seguro de enviar el formulario?',
-        type: 'warning',
+        icon: 'question',
         showCancelButton: true,
         confirmButtonText: 'Si',
         cancelButtonText: 'No',
-        confirmButtonColor: '#111B54',
-        cancelButtonColor: '#d33',
+        confirmButtonColor: '#0d6efd',
+        cancelButtonColor: '#6e7881',
     }).then((result) => {
         if (result.value) {
             document.formulario_publicar.submit();
@@ -16,4 +16,34 @@ function validar_formulario_publicar() {
         return false;
     })
     return false;
+}
+
+$('.btn-del').on('click',function(e){
+    e.preventDefault();
+    var id = $(this).attr('data-id');
+    Swal.fire({
+
+        title: '¿Estás Seguro?',
+        text : 'El aviso será eliminado',
+        type : 'question',
+        showCancelButton: true,
+        showConfirmButton: true,
+        confirmButtonColor : '#0d6efd',
+        cancelButtonColor: '#6e7881',
+        confirmButtonText : 'Eliminar aviso!',
+        closeOnConfirm: false,
+    }).then((result) => {
+        if(result.value){
+            console.log(href);
+            document.location.href = "../controlador/hu4_conserjeria_controlador/hu4_delete.php?="+id;
+        }
+    })
+})
+const flashdata = $('.flash-data').data('flashdata')
+if (flashdata){
+    Swal.fire({
+        type : 'succes',
+        title : 'Aviso eliminado',
+        text : 'De manera Exitosa',
+    })
 }
