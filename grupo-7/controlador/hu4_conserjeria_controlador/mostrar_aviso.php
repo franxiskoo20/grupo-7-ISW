12ace0ebe920cf56710e0f24ecae4f9c5d5df896
SoHenriquez <estebansotohenriquez@gmail.com>
2022-08-31 16:08:10 -0400

    Se actualiza consulta para especificar mostrar los avisos enviados

--- grupo-7/controlador/hu4_conserjeria_controlador/mostrar_aviso.php
+++ grupo-7/controlador/hu4_conserjeria_controlador/mostrar_aviso.php
@@ -1,7 +1,6 @@
 <?php
 require_once('../bds/conexion.php');
 
-
 $query = "SELECT U.usuario_nombre, U.usuario_apellido, U.usuario_correo, F.formulario_id,F.formulario_titulo,F.formulario_tipo,date_format(F.formulario_fecha, '%d-%m-%Y') AS fecha_formateada , F.formulario_hora, F.formulario_contenido 
             FROM formulario F, usuario U WHERE usuario_tipo= 'conserje' and formulario_remitente_id = usuario_id  ;";
 $stmt = $bd->prepare($query);
