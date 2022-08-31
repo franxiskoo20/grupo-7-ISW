41c1226b0b9f59e63b35ce7d41c567199d6b8416
SoHenriquez <estebansotohenriquez@gmail.com>
2022-08-31 15:06:55 -0400

    Se adapta consulta sql según lo requerido en tabla

--- grupo-7/controlador/hu4_conserjeria_controlador/mostrar_aviso.php
+++ grupo-7/controlador/hu4_conserjeria_controlador/mostrar_aviso.php
@@ -2,8 +2,8 @@
 require_once('../bds/conexion.php');
 
 
-$query = "SELECT F.formulario_id,F.formulario_titulo,F.formulario_tipo,date_format(F.formulario_fecha, '%d-%m-%Y') AS fecha_formateada , F.formulario_hora, F.formulario_contenido 
-            FROM formulario F WHERE  1  ;";
+$query = "SELECT U.usuario_nombre, U.usuario_apellido, U.usuario_correo, F.formulario_id,F.formulario_titulo,F.formulario_tipo,date_format(F.formulario_fecha, '%d-%m-%Y') AS fecha_formateada , F.formulario_hora, F.formulario_contenido 
+            FROM formulario F, usuario U WHERE usuario_tipo= 'conserje' and formulario_remitente_id = usuario_id  ;";
 $stmt = $bd->prepare($query);
 $stmt->execute();
 ?>
