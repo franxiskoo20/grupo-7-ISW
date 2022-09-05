e0d6bef61e337101ae8f11e4a3cd22ec2746e90c
SoHenriquez <estebansotohenriquez@gmail.com>
2022-09-05 13:22:39 -0300

    Se agrega función confirmación de modificación

--- grupo-7/js/hu4_conserjeria.js
+++ grupo-7/js/hu4_conserjeria.js
@@ -37,3 +37,23 @@ $(function(){
     });
     
 });
+/* Funcion para confirmar modificación */
+function validar_formulario_modificar() {
+    event.preventDefault();
+    Swal.fire({
+        title: '¿Seguro de enviar el formulario?',
+        icon: 'question',
+        iconColor: '#a5dc86',
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+        confirmButtonColor: '#0d6efd',
+        cancelButtonColor: '#6e7881',
+    }).then((result) => {
+        if (result.value) {
+            document.formulario_modificar.submit();
+        }
+        return false;
+    })
+    return false;
+}
