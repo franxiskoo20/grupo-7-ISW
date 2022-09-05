0d0ed8c6d7e6170cf275c08e5ad725a113244cc6
SoHenriquez <estebansotohenriquez@gmail.com>
2022-09-05 13:27:45 -0300

    Se actualiza consulta

--- grupo-7/controlador/hu4_conserjeria_controlador/hu4_read.php
+++ grupo-7/controlador/hu4_conserjeria_controlador/hu4_read.php
@@ -2,7 +2,7 @@
 require_once('../../bds/conexion.php');
 
 $query = "SELECT F.formulario_id,F.formulario_titulo,F.formulario_tipo,date_format(F.formulario_fecha, '%d-%m-%Y') AS fecha_formateada , F.formulario_hora, F.formulario_contenido 
-            FROM formulario F WHERE  1  ;";
+            FROM formulario F WHERE  F.formulario_tipo <> 'Reclamos' and  F.formulario_tipo <> 'Publicidad';";
 $stmt = $bd->prepare($query);
 $stmt->execute();
                             
