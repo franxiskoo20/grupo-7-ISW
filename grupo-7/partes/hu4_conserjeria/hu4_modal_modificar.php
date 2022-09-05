9559290fb463af07eb7a1ea3d1f01ef6f1e64551
SoHenriquez <estebansotohenriquez@gmail.com>
2022-09-05 12:52:53 -0300

    Se modifican limitaciones y se incorpora alerta de confirmación

--- grupo-7/partes/hu4_conserjeria/hu4_modal_modificar.php
+++ grupo-7/partes/hu4_conserjeria/hu4_modal_modificar.php
@@ -21,67 +21,81 @@
                         extract($row);
             ?>
             <?php if($_GET['id'] == $formulario_id){ ?>
+            <div class="modal-body mt-4">
             <!-- Formulario Crear nuevo Aviso de Conserjería -->
-            <form action="../controlador/hu4_conserjeria_controlador/hu4_edit.php"  method="POST">
-                <div class="modal-body mt-4">
-                    <div class="row">
-                        <div class="col">
-                            <div>
-                                <label >Seleccione tipo de registro</label>
+                <form class="was-validated" action="../controlador/hu4_conserjeria_controlador/hu4_edit.php"  method="POST" name="formulario_publicar" onsubmit="return validar_formulario_publicar()">
+                    
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-start">
+                                    <label >Seleccione tipo de registro</label>
+                                </div>
+                            </div>
+                            <div class="col"  >
+                                <select class="form-select is-invalid" name="tipo_form_actualizar" value="<?php echo $formulario_tipo ;?>" required>
+                                
+                                    <option value="" >Tipo de Registro </option>
+                                    <option value="Bitacora">Bitácora</option>
+                                    <option value="Encomienda">Encomienda</option>
+                                    <option value="Otro">Otro</option>
+                                </select>
+                                <div class="valid-feedback">
+                                    Aceptado.
+                                </div>
                             </div>
                         </div>
-                        <div class="col"  >
-                            <select class="form-select" name="tipo_form_actualizar" value="<?php echo $formulario_tipo ;?>" required>
-                            
-                                <option value="" >Tipo de Registro </option>
-                                <option value="Bitacora">Bitácora</option>
-                                <option value="Encomienda">Encomienda</option>
-                                <option value="Otro">Otro</option>
-                            </select>
+                        <!-- Modificar titulo y Fecha del aviso -->
+                        <div class="row">
+                            <div class="col text-start">
+                                <label for="exampleInputPassword1"  class="form-label">Título</label>
+                            </div>
+                            <div class="form-group col-12">
+                                <input type="text" placeholder="Ingrese Título -(10 - 50 caracteres)" value="<?php echo $formulario_titulo;?>" class="form-control is-invalid" name="titulo_actualizar" 
+                                    minlength="10" maxlength="50" required>
+                            </div> 
+                            <div class="valid-feedback">
+                                    Aceptado.
+                            </div>
                         </div>
-                    </div>
-                    <!-- Modificar titulo y Fecha del aviso -->
-                    <div class="row">
-                        <div class="form-group col">
-                            <label for="exampleInputPassword1"  class="form-label">Título</label>
-                            <input type="text" placeholder="Entrega de llaves" value="<?php echo $formulario_titulo;?>" class="form-control" name="titulo_actualizar" required>
-                        </div> 
+                        <br>
+                        <div class="row">
+                            <div class="col text-start">
+                                <label for="exampleInputPassword1" class="form-label">Fecha antigua: <?php echo $fecha_formateada;?></label>
+                            </div>
+                            <div class="col">
+                                <input type="date" class="form-control is-invalid" id="datepicker" name="fecha_actualizar" value="<?php echo $fecha_formateada;?>" required>
+                            </div>
+                            <div class="valid-feedback">
+                                    Aceptado.
+                            </div>
+                        </div>
+                        <br>
+                        <!-- Modificar Descripcion del Aviso -->
+                        <div class="row">
+                            <div class="form-group col-4 text-start">
+                                <label class="form-label ">Comentario</label>
+                            </div>
+                            <div class="col-8 w-100 campo" >
+                                <textarea class="input-text form-control"placeholder="Por favor, escriba un comentario para su aviso (10 - 900 caracteres)" name="descripcion_actualizar" rows="18" wrap="hard" minlength="10"
+                                maxlength="900"  style="height: 135px; width:100%;"><?php echo $formulario_contenido;?></textarea>
+                            </div>
+                        </div>
+                    
+                    <br>
+                    <div class="row text-end">
+                        <div class="col-7"></div>
+                        <div class="col-2"><button type="reset" class="btn btn-primary "><i class="fa-solid fa-trash"></i>&nbsp</button></div>
+                        <div class="form-group col-2">
+                            <button type="submit"  class="btn btn-primary">Enviar</button>
+                        </div>
                     </div>
                     <br>
-                    <div class="row">
-                        <div class="col">
-                            <label for="exampleInputPassword1" class="form-label">Fecha antigua: <?php echo $fecha_formateada;?></label>
-                        </div>
-                        <div class="col">
-                            <input type="date" class="form-control" id="datepicker" name="fecha_actualizar" value="<?php echo $fecha_formateada;?>" required>
-                        </div>
-                    </div>
-                    <br>
-                    <!-- Modificar Descripcion del Aviso -->
-                    <div class="row">
-                        <div class="form-group col-3">
-                            <label >Comentario</label>
-                        </div>
-                        <div class="col-7 campo" >
-                            <textarea class="input-text form-control" name="descripcion_actualizar" rows="18" wrap="hard" minlength="10"
-                            maxlength="1000"  style="height: 135px; width:100%;"><?php echo $formulario_contenido;?></textarea>
-                        </div>
-                    </div>
-                </div>
-                <br>
-                <div class="row text-end">
-                    <div class="col-7"></div>
-                    <div class="col-2"><button type="reset" class="btn btn-primary "><i class="fa-solid fa-trash"></i>&nbsp</button></div>
-                    <div class="form-group col-2">
-                        <button type="submit"  class="btn btn-primary">Enviar</button>
-                    </div>
-                </div>
-                <br>
-                <input type="hidden" name="form_clave" value="<?php echo $row['formulario_id']; ?>">
-                <?php }
-                }
-            }?>
-          </form>
+                    <input type="hidden" name="form_clave" value="<?php echo $row['formulario_id']; ?>">
+                    <?php }
+                        }
+                    }?>
+                </form>
+          </div>
         </div>
       </div>
     </div>
